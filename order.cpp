156d55c6e78e9c7ac7e8c6b71d4d30b73541c52e
Matt Calder <mcalder@lightkeeper.com>
2014-08-05 09:24:22 -0400

    Fixed string compare in order.cpp

--- order.cpp
+++ order.cpp
@@ -20,6 +20,7 @@
 
 extern "C" {
   PREFIX void OrderC_int(int* order, int* x, int len, int asc);
+  PREFIX void OrderC_long(int* order, long *x, int len, int asc);
   PREFIX void OrderC_double(int *order, double *x, int len, int asc);
   PREFIX void OrderC_double2(int *order, double *x, double *y, int len, int asc);
   PREFIX void OrderC_string(int* order, char** x, int len, int asc);
@@ -28,6 +29,16 @@ extern "C" {
 }
 
 void OrderC_int(int* order, int* x, int len, int asc)
+{   
+   for (int i=0; i < len; i++) {order[i] = i;}
+   if (asc) {
+     std::stable_sort(order, order + len, [&x](size_t i, size_t j) -> bool {return x[i] < x[j];});
+   } else {
+     std::stable_sort(order, order + len, [&x](size_t i, size_t j) -> bool {return x[i] > x[j];});
+   }
+}  
+
+void OrderC_long(int* order, long* x, int len, int asc)
 {   
    for (int i=0; i < len; i++) {order[i] = i;}
    if (asc) {
@@ -61,18 +72,32 @@ void OrderC_string(int* order, char** x, int len, int asc)
 void OrderC_double2(int *order, double *x, double *y, int len, int asc)
 {   
    for (int i=0; i < len; i++) {order[i] = i;}
-   std::stable_sort(order, order + len, [&x,&y](size_t i, size_t j) -> bool {return (x[i] == x[j]) ? (y[i] < y[j]) : (x[i] < x[j]);});
+   if (asc) {
+     std::stable_sort(order, order + len, [&x, &y](size_t i, size_t j) -> bool {return (x[i] == x[j]) ? (y[i] < y[j]) : (x[i] < x[j]);});
+   } else {
+     std::stable_sort(order, order + len, [&x, &y](size_t i, size_t j) -> bool {return (x[i] == x[j]) ? (y[i] > y[j]) : (x[i] > x[j]);});
+   }
 }  
 
 void OrderC_stringdate(int *order, char **x, long *y, int len, int asc)
 {   
-   for (int i=0; i < len; i++) {order[i] = i;}
-   std::stable_sort(order, order + len, [&x,&y](size_t i, size_t j) -> bool {return (x[i] == x[j]) ? (y[i] < y[j]) : (x[i] < x[j]);});
+  std::vector<std::string> str(len);
+  for (int i=0; i < len; i++) {order[i] = i; str[i] = x[i];}
+  if (asc) {
+    std::stable_sort(order, order + len, [&str, &y](size_t i, size_t j) -> bool {return (str[i] == str[j]) ? (y[i] < y[j]) : (str[i] < str[j]);});
+  } else {
+     std::stable_sort(order, order + len, [&str, &y](size_t i, size_t j) -> bool {return (str[i] == str[j]) ? (y[i] > y[j]) : (str[i] > str[j]);});
+  }
 }  
 
 void OrderC_wstringdate(int *order, wchar_t **x, long *y, int len, int asc)
 {   
-   for (int i=0; i < len; i++) {order[i] = i;}
-   std::stable_sort(order, order + len, [&x,&y](size_t i, size_t j) -> bool {return (x[i] == x[j]) ? (y[i] < y[j]) : (x[i] < x[j]);});
+  std::vector<std::wstring> str(len);
+  for (int i=0; i < len; i++) {order[i] = i; str[i] = x[i];}
+  if (asc) {
+    std::stable_sort(order, order + len, [&str, &y](size_t i, size_t j) -> bool {return (str[i] == str[j]) ? (y[i] < y[j]) : (str[i] < str[j]);});
+  } else {
+    std::stable_sort(order, order + len, [&str, &y](size_t i, size_t j) -> bool {return (str[i] == str[j]) ? (y[i] > y[j]) : (str[i] > str[j]);});
+  }
 }  
 
