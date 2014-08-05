49236e8d285c5c640bfe32852f1e86b71acfb59c
Matt Calder <mcalder@lightkeeper.com>
2014-08-05 12:20:42 -0400

    This time for real.

--- order_tests.cpp
+++ order_tests.cpp
@@ -10,8 +10,9 @@ std::vector<std::string> random_string(int size)
   std::default_random_engine generator;
   std::uniform_int_distribution<int> distribution(65, 90);
   for (int i=0; i < size; i++) {
-    char s[2] = {(char) distribution(generator), (char) distribution(generator)};
-    ans[i] = s[2];
+    ans[i] = std::string("  ");
+    ans[i][0] = (char) distribution(generator);
+    ans[i][1] = (char) distribution(generator);
   }
   return ans;
 }
@@ -38,19 +39,35 @@ bool test_string(int size)
   std::vector<int> order(size);
   OrderC_string(&order[0], &x[0], size, 1);
   for (int i=1; i < size; i++) {
-    ans = ans && (x[order[i]] >= x[order[i-1]]);
+    // printf("%s %i\n", x[order[i]], (int) ans);
+    ans = ans && (std::string(x[order[i]]) >= std::string(x[order[i-1]]));
   }
   return ans;
 }
 
 bool test_stringlong(int size)
 {
-  return true;
+  bool ans = true;
+  std::vector<std::string> strvec = random_string(size);
+  std::vector<const char *> x(size);
+  for (int i=0; i < size; i++) {
+    x[i] = strvec[i].c_str();
+  }
+  std::vector<long> lngvec = random_long(size);
+  std::vector<int> order(size);
+  OrderC_stringdate(&order[0], &x[0], &lngvec[0], size, 1);
+  for (int i=1; i < size; i++) {
+    // printf("%s %i %i\n", x[order[i]], lngvec[order[i]], ans);
+    ans = ans && ((std::string(x[order[i]]) > std::string(x[order[i-1]])) || (std::string(x[order[i]]) == std::string(x[order[i-1]]) && lngvec[order[i]] >= lngvec[order[i-1]]));
+    
+  }
+  return ans;
+  
 }
 
 int main(int argc, char**argv)
 {
-  int size = 10;
+  int size = 100000;
   printf("test_string = %s\n", test_string(size) ? "true" : "false");
-
+  printf("test_stringlong = %s\n", test_stringlong(size) ? "true" : "false");
 }
